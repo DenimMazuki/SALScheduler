4bb9443b0e7d52db760efae2803465542ad161f1
Denim Mazuki <denimmazuki@gmail.com>
2017-03-25 17:55:27 -0700

    Added UI sketch. TO-DO: Fix auto-layout

---
+++ SALScheduler/AddViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddViewController.swift
+//  SALScheduler
+//
+//  Created by Denim Mazuki on 3/25/17.
+//  Copyright © 2017 Denim Mazuki. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
